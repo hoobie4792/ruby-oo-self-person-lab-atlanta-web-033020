7d952e3d11cf333bc8b853027953ae750e53f74f
Brandon Brown <brandonbrown4792@gmail.com>
2020-04-01 15:37:46 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -30,6 +30,7 @@ class Person
   end
   
   def clean?
+<<<<<<< HEAD
     hygiene > 7
   end
   
@@ -39,33 +40,67 @@ class Person
   
   def get_paid(salary)
     self.bank_account += salary
+=======
+    @hygiene > 7
+  end
+  
+  def happy?
+    @happiness > 7
+  end
+  
+  def get_paid(amount)
+    @bank_account += amount
+>>>>>>> 253d15a0554bbbbfd599ffcb2c39b3a5cd6ac177
     "all about the benjamins"
   end
   
   def take_bath
+<<<<<<< HEAD
     self.hygiene = hygiene + 4
+=======
+    self.hygiene = @hygiene + 4
+>>>>>>> 253d15a0554bbbbfd599ffcb2c39b3a5cd6ac177
     "♪ Rub-a-dub just relaxing in the tub ♫"
   end
   
   def work_out
+<<<<<<< HEAD
     self.happiness = happiness + 2
     self.hygiene = hygiene - 3
+=======
+    self.happiness = @happiness + 2
+    self.hygiene = @hygiene - 3
+>>>>>>> 253d15a0554bbbbfd599ffcb2c39b3a5cd6ac177
     "♪ another one bites the dust ♫"
   end
   
   def call_friend(friend)
+<<<<<<< HEAD
     self.happiness = happiness + 3
     friend.happiness = friend.happiness + 3
     "Hi #{friend.name}! It's #{name}. How are you?"
+=======
+    self.happiness = @happiness + 3
+    friend.happiness = friend.happiness + 3
+    "Hi #{friend.name}! It's #{@name}. How are you?"
+>>>>>>> 253d15a0554bbbbfd599ffcb2c39b3a5cd6ac177
   end
   
   def start_conversation(person, topic)
     if topic == "politics"
+<<<<<<< HEAD
       self.happiness = happiness - 2
       person.happiness = person.happiness - 2
       "blah blah partisan blah lobbyist"
     elsif topic == "weather"
       self.happiness = happiness + 1
+=======
+      self.happiness = @happiness - 2
+      person.happiness = person.happiness - 2
+      "blah blah partisan blah lobbyist"
+    elsif topic == "weather"
+      self.happiness = @happiness + 1
+>>>>>>> 253d15a0554bbbbfd599ffcb2c39b3a5cd6ac177
       person.happiness = person.happiness + 1
       "blah blah sun blah rain"
     else
